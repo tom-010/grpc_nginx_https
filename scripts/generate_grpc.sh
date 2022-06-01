33f9219b679d7909c702a6f54f28823b72f6f860
Thomas Deniffel <tdeniffel@gmail.com>
2022-06-01 14:52:30 +0200

    go server-client example (not integrated with nginx)

---
+++ scripts/generate_grpc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "generate server"
+protoc \
+    --go_out=server \
+    --go_opt=paths=source_relative \
+    --go-grpc_out=server \
+    --go-grpc_opt=paths=source_relative \
+    proto/greeter.proto
+
+echo "generate go-clinet"
+protoc \
+    --go_out=client/go \
+    --go_opt=paths=source_relative \
+    --go-grpc_out=client/go \
+    --go-grpc_opt=paths=source_relative \
+    proto/greeter.proto
---
+++ scripts/run_client.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd client/go 
+go run client.go
